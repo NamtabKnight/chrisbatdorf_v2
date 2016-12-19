75ff2f3ad91452fbca5691158f320df59dc31f5b
NamtabKnight <cjbatdorf@gmail.com>
2016-12-18 23:20:17 -0500

    starting the explosion process

---
+++ common/footer.php
@@ -0,0 +1,53 @@
+<!-- Footer -->
+        <footer class="text-center">
+            <div class="footer-above">
+                <div class="container">
+                    <div class="row">
+                        <div class="footer-col col-md-4">
+                            <h3>Location</h3>
+                            <p><a href="https://www.google.com/maps/place/Holley,+NY+14470/@43.2262767,-78.0381703,15z/data=!3m1!4b1!4m5!3m4!1s0x89d406b0d620145d:0x7d7bbaa697f20910!8m2!3d43.2264486!4d-78.0266826" target="_new">Holley, NY 14470</a></p>
+                        </div>
+                        <div class="footer-col col-md-4">
+                            <h3>Around the Web</h3>
+                            <ul class="list-inline">
+                                <li>
+                                    <a href="https://www.facebook.com/pg/chrisbatdorfwebdevelopment" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                                </li>
+                                <!--                            <li>
+                                                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
+                                                            </li>-->
+                                <!--                            <li>
+                                                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
+                                                            </li>-->
+                                <li>
+                                    <a href="https://www.linkedin.com/in/chris-batdorf-24387444?trk=nav_responsive_tab_profile" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
+                                </li>
+                                <!--                            <li>
+                                                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
+                                                            </li>-->
+                            </ul>
+                            
+                            <ul class="list-inline">
+                                <li>
+                                    <a class="widget ng-scope" ng-href="https://www.thumbtack.com/-Holley-NY/service/2709895" target="_blank" href="https://www.thumbtack.com/-Holley-NY/service/2709895"> <img style="" ng-src="https://static.thumbtackstatic.com/media/pages/profile/standard-widgets/pro-svg/inline.svg" src="https://static.thumbtackstatic.com/media/pages/profile/standard-widgets/pro-svg/inline.svg"></a><script src='https://www.thumbtack.com/profile/widgets/scripts/?service_id=g2AijfZJ9jC4Ww&widget_id=profile'></script>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                        <div class="footer-col col-md-4">
+                            <h3>About Me In Review</h3>
+                            <p>I'm a Web Developer for hire. I specialize in building solutions for small to medium size businesses.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-below">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            Copyright &copy; Chris Batdorf 2016
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
---
+++ index.php
@@ -0,0 +1,131 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Chris Batdorf is a Web Developer and Web Designer from Holley, NY with years of experience in web development and design, seo, and eCommerce.">
+        <meta name="keywords" content="Web Development, Web Designer, Holley, NY, Rochester, New York, Buffalo, SEO, Analytics">
+        <meta name="author" content="Chris Batdorf">
+
+        <title>Chris Batdorf - Web Development and Web Design</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Theme CSS -->
+        <link href="css/freelancer.min.css" rel="stylesheet">
+        <link href="css/main.css" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <?php include 'common/analytics.php'; ?>
+    </head>
+    <body id="page-top" class="index">
+        <?php include 'common/nav.php'; ?>
+        
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <!--<img class="img-responsive" src="assets/profile.png" alt="">-->
+                        <!--<img class="img-responsive" src="assets/logo/logo_rounded_v3_275_grey.gif" alt="Chris Batdorf Logo">-->
+                        <img class="img-responsive" src="assets/logo/logo_rounded_v3_275_orange.gif" alt="Chris Batdorf Logo">
+                        <div class="intro-text">
+                            <h1><span class="name"><span style="color:#04B3CD;">Chris</span> <span style="color:#ED5523;">Batdorf</span></span></h1>
+                            <hr class="star-light">
+                            <h2><span class="skills">Web Developer - SEO - eCommerce - Analytics</span></h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        
+        <!-- What Am I Section -->
+        <section class="success" id="what">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2>What Am I?</h2>
+                        <hr class="star-light">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <p>I am a developer that provides full Web Development services. Everything from domain / hosting, to design and implementation, to SEO and Analytics. I work with each client to tailor a solution that not only works for them now, but incorporates future plans as well.</p>
+                    
+                        <p>I have years of experience creating both hand coded, W3C compliant websites as well as utilizing popular frameworks such as WordPress, LightCMS, Bootstrap, and Joomla to create both informational and eCommerce Solutions.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-lg-offset-2">
+                        <h4>Available <br>Technologies:</h4>
+                        <ul>
+                            <li><a href="https://wordpress.org/" target="_new">WordPress</a></li>
+                            <li><a href="https://www.joomla.org/" target="_new">Joomla</a></li>
+                            <li><a href="https://magento.com/" target="_new">Magento</a></li>
+                            <li><a href="https://www.opencart.com/" target="_new">OpenCart</a></li>
+                            <li><a href="https://www.getbootstrap.com/" target="_new">Bootstrap</a></li>
+                            <li>Hand Built Websites</li>
+                            <li>API's</li>
+                            <li>eCommerce</li>
+                            <li>Social Media Integrations</li>
+                            <li>and more...</li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-4">
+                        <h4><br>Services:</h4>
+                        <ul>
+                            <li>SEO</li>
+                            <li>Responsive Design</li>
+                            <li>Web Hosting</li>
+                            <li>Domain Management</li>
+                            <li>Flexible Monthly Maintenance Plans</li>
+                            <li>Newsletter Setup</li>
+                            <li>Free Quotes</li>
+                            <li>Maintenance on Existing Websites</li>
+                            <li>Responsive Redesigns to existing websites</li>
+                            <li>and more...</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <?php include 'common/footer.php'; ?>
+        <?php include 'common/belowFooter.php'; ?>
+        
+                <!-- jQuery -->
+        <script src="vendor/jquery/jquery.min.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+        <!-- Plugin JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+        <!-- Contact Form JavaScript -->
+        <script src="js/jqBootstrapValidation.js"></script>
+        <script src="js/contact_me.js"></script>
+
+        <!-- Theme JavaScript -->
+        <script src="js/freelancer.min.js"></script>
+
+    </body>
+</html>
